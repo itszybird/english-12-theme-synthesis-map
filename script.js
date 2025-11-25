319fc84087508b421ae0a6eadc6580a0f5861d91
ZYYY-PRO <compt20pro@gmail.com>
2025-11-24 22:24:15 -0800

    v1

---
+++ script.js
@@ -0,0 +1,263 @@
+/* ===== DATA ===== */
+const nodes = [
+    {
+    id: "Intro",
+    title: ".   Intro",
+    note: [
+        `<h2>Howdy hey!</h2>
+        Im used Canva too many times so I decided to code my own graphical website for my synthesis project.
+        This is a graphical website I coded to display the synthesis, because I couldn’t think of any other way to use my time IDK, ok but like it took me almost 6 hours to program this from ground up using Html, CSS, and JavaScript.
+
+        To navigate this site, it's simple:
+        • All you need to do is follow the lines connected in the map.
+        • After reading the theme, choose either left or right.
+        • Click on the circles to read the notes.
+        • Drag across the map to move around.
+        • Use the scroll wheel to zoom in and out.
+
+        <b>My synthesis theme poster is based on the two souces:</b>
+        • <i>The video:</i> "I'm Not Your Inspiration, Thank You Very Much" | Stella Young
+        • <i>The story:</i> "Canfei to Canji: The Freedom of Being Loud" | Sandy Ho
+
+        Please enjoy playing through the map!
+        
+        <i>Move down to start!</i>
+        `
+    ]
+    },
+
+    {
+    id: "Theme Statement",
+    title: ".   Theme Statement",
+    note: [
+        `<b>The Problem of Misunderstanding information</b>
+
+        A <b>common</b> theme between Stellar Young's “I’m not your inspiration” and Sandy Ho’s “Canfei to Canji” is that people often misunderstand information, because society/social media makes assumptions about them instead of listening to or watching their real experiences. Through both the video and text, they expressively focus on disabled people being put into narrow roles to become so-called “inspirations” and completely ignore their individual identities. Young talks about how the world limits disabled people as “inspiration porn” and ignores the reality of their lives. Similarly, Ho's experiences of cultural stigma and silence create incorrect assumptions about disabled people’s worth. Combined, the video and the text highlight how powerful society’s labels or more present “social media,” can be in harming how others make decisions and the skills of critical thinking. From comparing the two, we can gain a better understanding of how assumptions shape the way disabled people are treated. Ultimately, both authors argue that to understand something is to listen to the official source rather than present social media.
+        
+        <i>Move left or right!</i>
+        `        
+    ]
+    },
+
+    {
+    id: "Stella Young quote 1",
+    title: ".   Stella Young quote 1",
+    note: [
+        `<a href="https://youtu.be/8K9Gg164Bsw?si=_9-3pv6N817togmo&t=287" target="_blank">“They were just kind of congratulating me for managing to get up in the morning and remember my own name.” - Stella Young</a>
+
+        Young talks about getting <b>congratulated</b> for “managing to get up in the morning and remember [her] own name” (Young, Link). This quote shows how society/social media assumes about disabled people instead of actually understanding their identities. Young explains that teachers or students praise her for doing very simple human tasks, though, revealing that society’s expectations are underrated. Assumptions like these come from an ideology that disabled people are meant to struggle continuously, even to accomplish simple tasks, such as waking up. This links to the theme because people would see only her physical body, rather than her identities and experiences. This results in society deciding her value for her. Young uses this example to emphasize how deeply misunderstanding shapes how people form opinions.
+        
+        <i>Move right!</i>
+        `        
+    ]
+    },
+
+    {
+    id: "Sandy Ho quote 1",
+    title: ".   Sandy Ho quote 1",
+    note: [
+        `<b>“The Chinese Language shaped the perceptions of disability… wasn’t until the 1990s… canfei (useless) to canji (sickness or illness)” - Sandy Ho</b>
+
+        <b>Towards</b> More Cultural Aspects, Ho explains how “The Chinese Language shaped the perceptions of disability… wasn’t until the 1990s… canfei (useless) to canji (sickness or illness)”(Ho, 3). This quote shows how cultural languages shape assumptions about disabled people before they even speak. Especially when disability is linked to negative words, society shifts to believing it is truly a sin to be disabled. Ho’s own experiences show that these inherited ideologies influence families and communities, learning that disability should be avoided and feared. This links to the theme because it emphasizes how misunderstanding is also created through languages and cultural aspects, not from disabled people themselves. Society makes assumptions from cultural words rather than experiences. Ho argues that this shows how harmful language and words are towards not only disabled people, but also for many people in communities.
+        
+        <i>Move left!</i>
+        `        
+    ]
+    },
+
+    {
+    id: "Stella Young quote 2",
+    title: ".   Stella Young quote 2",
+    note: [
+        `<a href="https://youtu.be/8K9Gg164Bsw?si=_fR6Lel9vu9yaclU&t=170" target="_blank">“We've been sold the lie that disability is a Bad Thing, capital B, capital T. It's a bad thing, and to live with a disability makes you exceptional. It's not a bad thing, and it doesn't make you exceptional. And in the past few years, we've been able to propagate this lie even further via social media.” - Stella Young</a>
+
+        Young here explains that “we’ve been <b>sold the lie</b> that disability is a Bad Thing… It’s not a bad thing, and it doesn’t make you exceptional… even further via social media” (Young, Link). This segment challenges the stereotype that disabled people must be “exceptional” simply for even existing. Society creates assumptions that disability must automatically mean brokenness, weakness, tragedy, or a way of inspiration and ignores the real variety and complexity in disabled people’s identities. Instead of listening to true information, society/social media such as Instagram puts disabled people into motivational inspiration for others, and it's usually misunderstood by many people. This supports the theme because it proves how, not just for the disabled, but for many other factors, people rely on stereotypical information rather than real stories and experiences. Young emphasizes that social media and the world’s mindset strip disabled people of their identities as humans.
+        
+        <i>Picture Below!</i>
+        `        
+    ]
+    },
+
+    {
+    id: "Sandy Ho quote 2",
+    title: ".   Sandy Ho quote 2",
+    note: [
+        `<b>“Taking up space as a disabled person is always revolutionary” - Sandy Ho</b>
+
+        <b>As a disabled person,</b> most of society will see you “Taking up space as a disabled person [being] always revolutionary” (Ho, 4). This quote expresses how just being a disabled person in the current world challenges society’s assumptions. Ho explains how many disabled people are expected to be silent, hidden, or grateful, but if they choose to take up space in communities and society, the barrier of misunderstandings will contract. This connects to the theme since if one steps up and becomes heard by others, this interrupts the unbalanced society’s assumptions and forces people to view real-life experiences. Ho creates this idea to express that disabled people deserve to be visible and understood to be treated as part of basic humanity and not assumed by social contradictions.
+        `        
+    ]
+    },
+
+    {
+    id: "Stella Young picture",
+    title: ".   Stella Young picture",
+    note: [
+        `<b>“Taking up space as a disabled person is always revolutionary” - Sandy Ho</b>
+
+        <img src="stella.png" style="width: 300px; border-radius: 12px; margin-bottom: 12px;">
+
+
+
+        `        
+    ]
+    },
+
+    {
+    id: "Sandy Ho picture",
+    title: ".   Sandy Ho picture",
+    note: [
+        `<b>“Taking up space as a disabled person is always revolutionary” - Sandy Ho</b>
+
+        <img src="stella.png" style="width: 300px; border-radius: 12px; margin-bottom: 12px;">
+
+        
+
+        `        
+    ]
+    },
+
+    {
+    id: "End",
+    title: ".   End",
+    note: [
+        `<h2>Howdy hey! You found me!</h2>
+        <h3>Reflection:</h3>
+        Overall, learning about this theme helped guide me to understand that the power of assumptions from only viewing the surface of identity can hugely affect society’s opinions. Both Young and Ho showed explicit content about disabled people, and they stated that people often think they understand disability without ever experiencing it or listening to someone with a disability. I myself have experienced being disabled, but in the form of having a younger brother. Though from this experience I can confirm that both authors are correct. When people with disabilities do normal tasks, it should not be a congratulatory accomplishment, but rather a basic human task. Although they require tools, they still manage to do daily tasks effortlessly in different ways (such as my younger brother using hearing aids). Looking towards the future, I am more confident when approaching people with disabilities through thinking, watching, and listening. Finally, of all wisdom achieved, I learned that to understand someone or something, it is essential to find official sources or do research upon discovery, before believing. Nothing on social media is completely true, which is why no one should make assumptions at first sight.
+        
+        <i>Thats it! THANK YOU! 😀</i>
+        `
+    ]
+    }
+    
+];
+
+const links = [
+    { source: "Intro", target: "Theme Statement" },
+    { source: "Theme Statement", target: "Stella Young quote 1" },
+    { source: "Theme Statement", target: "Sandy Ho quote 1" },
+    { source: "Stella Young quote 1", target: "Stella Young quote 2" },
+    { source: "Sandy Ho quote 1", target: "Sandy Ho quote 2" },
+    { source: "Stella Young picture", target: "Stella Young quote 2" },
+    { source: "Stella Young picture", target: "Stella Young quote 1" },
+    { source: "Sandy Ho picture", target: "Sandy Ho quote 2" },
+    { source: "Sandy Ho picture", target: "Sandy Ho quote 1" },
+    { source: "End", target: "End" }
+    
+];
+
+/* ===== GRAPH SETUP ===== */
+const svg = d3.select("#graph");
+const width = window.innerWidth * 0.1;
+const height = window.innerHeight * 0.1;
+svg.attr("viewBox", [-30, -230, width, height]);
+
+
+const linkGroup = svg.append("g").attr("class", "links");
+const nodeGroup = svg.append("g").attr("class", "nodes");
+const labelGroup = svg.append("g").attr("class", "labels");
+
+const link = linkGroup
+    .selectAll("line")
+    .data(links)
+    .join("line")
+    .attr("class", "link");
+
+const node = nodeGroup
+    .selectAll("circle")
+    .data(nodes)
+    .join("circle")
+    .attr("class", "node-circle")
+    .attr("r", 9)
+    .attr("r", d => d.id === "End" ? 300 : 9)
+    .on("click", (_, d) => selectNode(d))
+    .call(
+    d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
+
+const label = labelGroup
+    .selectAll("text")
+    .data(nodes)
+    .join("text")
+    .attr("class", "node-label")
+    .text(d => d.title);
+
+const simulation = d3.forceSimulation(nodes)
+    .force("link", d3.forceLink(links).id(d => d.id).distance(400))
+    .force("charge", d3.forceManyBody().strength(-2000))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .force("collide", d3.forceCollide())
+    .on("tick", ticked);
+
+function ticked() {
+    link
+    .attr("x1", d => d.source.x)
+    .attr("y1", d => d.source.y)
+    .attr("x2", d => d.target.x)
+    .attr("y2", d => d.target.y);
+
+    node
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y);
+
+    label
+    .attr("x", d => d.x + 8)
+    .attr("y", d => d.y + 3);
+}
+
+const zoom = d3.zoom()
+  .scaleExtent([0.05, 3])
+  .on("zoom", ({ transform }) => {
+    linkGroup.attr("transform", transform);
+    nodeGroup.attr("transform", transform);
+    labelGroup.attr("transform", transform);
+  });
+
+svg.call(zoom);
+
+function dragstarted(event, d) {
+    if (!event.active) simulation.alphaTarget(0.2).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragged(event, d) {
+    d.fx = event.x;
+    d.fy = event.y;
+}
+
+function dragended(event, d) {
+    if (!event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+}
+
+
+
+/* ===== NOTE PANEL ===== */
+const noteTitle = document.getElementById("note-title");
+const noteMeta = document.getElementById("note-meta");
+const noteBody = document.getElementById("note-body");
+
+function selectNode(d) {
+    node.classed("active", n => n.id === d.id);
+    noteTitle.textContent = d.title;
+    noteMeta.textContent = "Node id: " + d.id;
+    noteBody.innerHTML = d.note;
+}
+
+selectNode(nodes[0]);
+
+noteBody.addEventListener("selectstart", (e) => {
+  e.preventDefault();
+});
+
+noteBody.addEventListener("mousedown", () => {
+  const sel = window.getSelection();
+  if (sel && sel.rangeCount > 0) {
+    sel.removeAllRanges();
+  }
+});
